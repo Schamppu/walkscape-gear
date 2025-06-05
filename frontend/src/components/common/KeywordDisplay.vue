4cb42a35fc166af32206c5f2c04b05b29a4fcb28
Juhana Autti <ext-juhana.autti@3stepit.com>
2025-06-05 20:06:27 +0300

    add KeywordDisplay

---
+++ frontend/src/components/common/KeywordDisplay.vue
@@ -0,0 +1,33 @@
+<script setup>
+import WsIcon from "./WsIcon.vue";
+
+const props = defineProps({
+  keyword: Object,
+});
+</script>
+
+<template>
+  <div class="keyword-display">
+    <ws-icon v-if="keyword.icon" :iconPath="keyword.icon" />
+    <p class="text">
+      <span v-if="keyword.quantity">{{ keyword.quantity }}</span>
+      {{ keyword.name }}
+    </p>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.keyword-display {
+  display: flex;
+  align-items: center;
+  gap: $xxs;
+  border-radius: $sm;
+
+  background-color: $boxTransparentDarkBackground;
+  padding: $xxxxs $md $xxxxs $xxxxs;
+
+  .text {
+    text-wrap: nowrap;
+  }
+}
+</style>
