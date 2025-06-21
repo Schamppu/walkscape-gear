d235ea37056992c6000ac6e2572892bbb564effd
Juhana Autti <juhana.autti@gmail.com>
2025-06-21 18:10:42 +0300

    increase dropdown item width

--- frontend/src/components/common/dropdowns/DropdownItem.vue
+++ frontend/src/components/common/dropdowns/DropdownItem.vue
@@ -17,7 +17,7 @@ function handleClick() {
 
 <template>
   <li>
-    <button class="dropdown-item" @click="handleClick"  >
+    <button class="dropdown-item" @click="handleClick">
       <LabelWithIcon :icon="item.icon" :text="item.value" />
     </button>
   </li>
@@ -26,6 +26,7 @@ function handleClick() {
 <style lang="scss" scoped>
 .dropdown-item {
   cursor: pointer;
+  width: 100%;
   padding: $xxs 0;
   transition: background 0.2s ease;
   background-color: $boxTransparentDarkBackground;
