d4ff7c6b22b4f8090806fc61a3f63d34a9fa8e44
Juhana Autti <juhana.autti@gmail.com>
2025-07-01 19:54:26 +0300

    add factionReputation DB functions

--- backend/src/controllers/dbController.cjs
+++ backend/src/controllers/dbController.cjs
@@ -96,9 +96,64 @@ const upsertUserOwnedItems = async (req, res) => {
   }
 };
 
+const getUserFactionReputations = async (req, res) => {
+  const userUuid = req.headers["x-user-uuid"];
+  if (!userUuid) return res.status(400).json({ error: "Missing userUuid" });
+
+  const reputations = await prisma.factionReputation.findMany({
+    where: { userUuid },
+  });
+
+  const mappedItems = Object.fromEntries(
+    reputations.map(({ reputation, value }) => [reputation, value])
+  );
+
+  res.json(mappedItems || {});
+};
+
+const upsertUserFactionReputations = async (req, res) => {
+  const userUuid = req.headers["x-user-uuid"];
+  if (!userUuid) return res.status(400).json({ error: "Missing userUuid" });
+
+  // Check if user exists
+  const user = await prisma.user.findUnique({ where: { userUuid } });
+
+  if (!user) {
+    await prisma.user.create({ data: { userUuid } });
+  }
+
+  const { reputations } = req.body;
+
+  try {
+    await Promise.all(
+      Object.entries(reputations).map(([reputation, value]) =>
+        prisma.factionReputation.upsert({
+          where: { userUuid_reputation: { userUuid, reputation } },
+          update: {
+            reputation,
+            value,
+          },
+          create: {
+            userUuid,
+            reputation,
+            value,
+          },
+        })
+      )
+    );
+
+    res.sendStatus(200);
+  } catch (e) {
+    console.error(e);
+    res.sendStatus(500);
+  }
+};
+
 module.exports = {
   getUserInfo,
   upsertUserInfo,
   getUserOwnedItems,
   upsertUserOwnedItems,
+  getUserFactionReputations,
+  upsertUserFactionReputations,
 };
--- backend/src/routes/dbRoutes.js
+++ backend/src/routes/dbRoutes.js
@@ -4,6 +4,8 @@ import {
   upsertUserInfo,
   getUserOwnedItems,
   upsertUserOwnedItems,
+  getUserFactionReputations,
+  upsertUserFactionReputations,
 } from "../controllers/dbController.cjs";
 
 const router = Router();
@@ -12,5 +14,7 @@ router.get("/user_stats", getUserInfo);
 router.post("/user_stats", upsertUserInfo);
 router.get("/owned_items", getUserOwnedItems);
 router.post("/owned_items", upsertUserOwnedItems);
+router.get("/faction_reputations", getUserFactionReputations);
+router.post("/faction_reputations", upsertUserFactionReputations);
 
 export { router as dbRoutes };
