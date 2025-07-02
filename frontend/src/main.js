d01d3a912bcf2ab93069be813b2c389e3553fd42
Juhana Autti <juhana.autti@gmail.com>
2025-07-02 15:33:04 +0300

    remove ElementPlus and old component

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,15 +3,10 @@ import { createPinia } from "pinia";
 import directives from "@/directives";
 import App from "./App.vue";
 
-// Import Element Plus and its CSS
-import ElementPlus from "element-plus";
-import "element-plus/dist/index.css";
-
 const app = createApp(App);
 const pinia = createPinia();
 
 app.use(pinia);
-app.use(ElementPlus);
 
 Object.entries(directives).forEach(([name, directive]) => {
   app.directive(name, directive);
