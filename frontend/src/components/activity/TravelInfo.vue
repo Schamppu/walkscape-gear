91aac073c20f32749fcc66879dbc5acd61f5299e
Juhana Autti <juhana.autti@gmail.com>
2025-08-01 10:31:18 +0300

    add base for TravellingInfo

---
+++ frontend/src/components/activity/TravelInfo.vue
@@ -0,0 +1,25 @@
+<script setup></script>
+
+<template>
+  <details open>
+    <summary>Travel Info</summary>
+
+    <section :class="['travel-info', 'border-agility']">
+      <p>This is still being developed.</p>
+    </section>
+  </details>
+</template>
+
+<style lang="scss" scoped>
+.travel-info {
+  border-radius: $md;
+  display: flex;
+
+  flex-wrap: wrap;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: $lg;
+
+  padding: $md;
+}
+</style>
