90bcbb878779b035bda5fe8c0dbf609a62bbdce8
Juhana Autti <juhana.autti@gmail.com>
2025-12-13 19:19:58 +0200

    rename useBaseContext, add gear

--- frontend/src/composables/useBaseContext.js
+++ frontend/src/composables/useBaseContext.js
@@ -2,11 +2,13 @@ import { computed } from "vue";
 import { useActivityStore } from "@/store/activity";
 import { usePlayerStore } from "@/store/player";
 import { useItemsStore } from "@/store/items";
+import { useGearStore } from "@/store/gear";
 
-function useBaseCalculationContext() {
+function useBaseContext() {
   const activityStore = useActivityStore();
   const playerStore = usePlayerStore();
   const itemsStore = useItemsStore();
+  const gearStore = useGearStore();
 
   return {
     activitySelected: computed(() => activityStore.activitySelected),
@@ -18,7 +20,9 @@ function useBaseCalculationContext() {
     skillLevels: computed(() => playerStore.skillLevels),
 
     items: computed(() => itemsStore.allItems),
+
+    gearSlots: computed(() => gearStore.gearSlots),
   };
 }
 
-export default useBaseCalculationContext;
+export default useBaseContext;
