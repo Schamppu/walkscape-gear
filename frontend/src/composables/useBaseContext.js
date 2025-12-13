098d4c0eedf2ae3959e87e0800473fda104dbead
Juhana Autti <juhana.autti@gmail.com>
2025-12-13 19:52:22 +0200

    expand useBaseContext

--- frontend/src/composables/useBaseContext.js
+++ frontend/src/composables/useBaseContext.js
@@ -1,27 +1,39 @@
 import { computed } from "vue";
 import { useActivityStore } from "@/store/activity";
-import { usePlayerStore } from "@/store/player";
-import { useItemsStore } from "@/store/items";
 import { useGearStore } from "@/store/gear";
+import { useItemsStore } from "@/store/items";
+import { usePlayerStore } from "@/store/player";
+import { useRouteStore } from "@/store/route";
 
 function useBaseContext() {
   const activityStore = useActivityStore();
-  const playerStore = usePlayerStore();
-  const itemsStore = useItemsStore();
   const gearStore = useGearStore();
+  const itemsStore = useItemsStore();
+  const playerStore = usePlayerStore();
+  const routeStore = useRouteStore();
 
   return {
     activitySelected: computed(() => activityStore.activitySelected),
     recipeSelected: computed(() => activityStore.recipeSelected),
 
-    activity: computed(() => activityStore.activity),
-    recipe: computed(() => activityStore.recipe),
+    activity: computed(
+      () => activityStore.activitySelected && activityStore.activity
+    ),
+    recipe: computed(
+      () => activityStore.recipeSelected && activityStore.recipe
+    ),
+    location: computed(() => activityStore.location),
 
     skillLevels: computed(() => playerStore.skillLevels),
+    achievementPoints: computed(() => playerStore.achievementPoints),
+    factionReputation: computed(() => playerStore.factionReputation),
 
     items: computed(() => itemsStore.allItems),
 
     gearSlots: computed(() => gearStore.gearSlots),
+    equippedGear: computed(() => gearStore.equippedGear),
+
+    segments: computed(() => routeStore.segments),
   };
 }
 
